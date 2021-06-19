5c2bec7fafd26c63ec5eb0a604b96f8cc41c112f
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-06-19 13:23:51 +0100

    added express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 8080;
+const APPINDEX = 'web-build/index.html';
+
+const server = express()
+  .use(express.static('web-build'))
+  .get('/', (req, res) => res.sendFile(APPINDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
