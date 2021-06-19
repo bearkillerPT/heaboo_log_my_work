3c06015816eca8dfbbda7b3ce7d1d2353a10a498
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-06-19 15:48:11 +0100

    trying so hard

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { Server } = require('ws');
 const path = require('path')
 const PORT = process.env.PORT || 8080;
-const APPINDEX = 'web-build/index.html';
+const APPINDEX = './web-build/index.html';
 const server = express()
-  .use(express.static('web-build'))
+  .use(express.static('./web-build'))
   .get('/', (req, res) => res.sendFile(APPINDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-express().set('strict routing', true);
 
 const wss = new Server({ server });
 
