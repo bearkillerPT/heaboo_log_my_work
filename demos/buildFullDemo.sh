832382392b1f944f35a26668ef75d2f7e411f9a5
Gil Teixeira <gilteixeira@ua.pt>
2021-07-12 16:22:55 +0100

    fixed auto gen script

--- demos/buildFullDemo.sh
+++ demos/buildFullDemo.sh
@@ -1,2 +1,3 @@
-ffmpeg -i iphone_demo.mp4 -vf pad="ceil(iw/2)*2:1080:y=300" output.mp4 -preset slow
+ffmpeg -i iphone_demo.mp4 -vf pad="ceil(iw/2)*2:1080:y=300" iphone1080.mp4 -preset slow
 ffmpeg -i website_demo.mp4 -i iphone1080.mp4 -filter_complex hstack fullDemo.mp4 -preset slow
+rm iphone1080.mp4
