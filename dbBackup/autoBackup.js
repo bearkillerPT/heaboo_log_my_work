4806de9fa3a28f5b962191c6a74af813ecb54e33
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-07-14 16:00:07 +0100

    adutoBackup completed and set to twice every day

--- dbBackup/autoBackup.js
+++ dbBackup/autoBackup.js
@@ -1,36 +1,40 @@
-import firebase from 'firebase-admin';
-import {firebaseConfig} from './firebaseConfig.js';
-setTimeout(
-  () => {
-    const fs = require('fs');
-    let path = "dbBackup/" + Date.now().toString() + '.json';
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    } else {
-      firebase.app(); // if already initialized, use that one
-    }
-    const database = firebase.database();
-    let backupContent = database.ref('./').once('value')
-    .then(
-      (val) => {
-        fs.open(path, 'w', function (err, fd) {
-          if (err) {
-            throw 'could not open file: ' + err;
-          }
-    
-          // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
-          fs.write(fd, val.val(), 0, val.val().toString().length, null, function (err) {
-            if (err) throw 'error writing file: ' + err;
-            fs.close(fd, function () {
-              console.log('wrote the file successfully');
-            });
-          });
-        });
-      }
-    )
-    .catch(console.log);
-    // open the file in writing mode, adding a callback function where we do the actual writing
-    
+const { json } = require('body-parser');
+const { white } = require('color-name');
+var firebase = require('firebase-admin');
+var serviceAccount = require('./serviceAccountKey.json');
+
+
+
+const sleep = async(ms) =>{
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+const doBackup = async () => {
+  const fs = require('fs');
+  let path = Date.now().toString() + '.json';
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(serviceAccount),
+      databaseURL: "https://log-my-work-8db34-default-rtdb.firebaseio.com"
+    });
+  } else {
+    firebase.app(); // if already initialized, use that one
   }
-  , 43200
-);
+  
+  const database = firebase.database();
+  console.log(database)
+
+  database.ref('/users/').once('value',
+    (val) => {
+      console.log(JSON.stringify(val.val()))
+      fs.writeFileSync(__dirname + "/" + path, JSON.stringify(val.val()), err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })
+    })
+}
+
+setInterval(doBackup, 43200000);
